2483571d0fa585d2b9a1a25caeb024d6edab6d62
Ahmad Mtera <ahmadmtera@laptop.local>
2024-06-19 20:54:48 +0300

    Edited import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,6 @@ USE hms;
 insert into role (name) values ("ROLE_ADMIN");
 insert into role (name) values ("ROLE_CUSTOMER");
 insert into role (name) values ("ROLE_EMPLOYEE");
+insert into _user (dtype, email, name, password_hash) values ("Employee", "ahmad@email.com", "Ahmad", "$2a$10$p9Ib.UcUK18pMPCIcuabxe1GXJd2kEeTWtWcGX8C7sIL4oKsgZGMG");
+insert into user_role (role_id, user_id) values (1, 1);
+insert into user_role (role_id, user_id) values (3, 1)
